e9ca7a5b4ee22b51621351a7a76e6bcf05c4a3c7
Stephen Shank <stephenshank@google.com>
2019-02-19 17:54:40 -0800

    Add listClusters to ContainerClient. Refactor ContainerClientTest. Add tests for listClusters.

--- src/test/java/com/google/jenkins/plugins/k8sengine/client/ContainerClientTest.java
+++ src/test/java/com/google/jenkins/plugins/k8sengine/client/ContainerClientTest.java
@@ -16,108 +16,188 @@ package com.google.jenkins.plugins.k8sengine.client;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import com.google.api.services.container.Container;
+import com.google.api.services.container.Container.Projects.Zones.Clusters;
 import com.google.api.services.container.model.Cluster;
+import com.google.api.services.container.model.ListClustersResponse;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-import org.junit.Before;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 /** Tests {@link ContainerClient}. */
 @RunWith(MockitoJUnitRunner.class)
 public class ContainerClientTest {
-  private ContainerClient containerClient;
-  @Mock private Container.Projects.Zones.Clusters clusters;
+  private static final String TEST_PROJECT_ID = "test-project-id";
+  private static final String OTHER_PROJECT_ID = "other-project-id";
+  private static final String ERROR_PROJECT_ID = "error-project-id";
+  private static final String EMPTY_PROJECT_ID = "empty-project-id";
+  private static final String TEST_ZONE = "us-west1-a";
+  private static final String OTHER_ZONE = "us-west2-b";
+  private static final String TEST_CLUSTER = "testCluster";
+  private static final String OTHER_CLUSTER = "otherCluster";
 
-  @Before
-  public void init() throws Exception {
+  private static ContainerClient containerClient;
+  private static Cluster testCluster;
+  private static Cluster otherCluster;
+
+  @BeforeClass
+  public static void init() throws Exception {
     Container container = Mockito.mock(Container.class);
-    ;
     Container.Projects projects = Mockito.mock(Container.Projects.class);
     Container.Projects.Zones zones = Mockito.mock(Container.Projects.Zones.class);
+    Clusters clusters = Mockito.mock(Container.Projects.Zones.Clusters.class);
     Mockito.when(container.projects()).thenReturn(projects);
     Mockito.when(projects.zones()).thenReturn(zones);
     Mockito.when(zones.clusters()).thenReturn(clusters);
-    this.containerClient = new ContainerClient(container);
+    testCluster = new Cluster().setName(TEST_CLUSTER).setZone(TEST_ZONE);
+    otherCluster = new Cluster().setName(OTHER_CLUSTER).setZone(TEST_ZONE);
+
+    Mockito.when(clusters.get(anyString(), anyString(), anyString()))
+        .thenAnswer(
+            mockClustersGetAnswer(
+                new ImmutableMap.Builder<String, ImmutableList<Cluster>>()
+                    .put(TEST_PROJECT_ID, ImmutableList.of(testCluster))
+                    .build()));
+
+    Mockito.when(clusters.list(anyString(), anyString()))
+        .thenAnswer(
+            mockClustersListAnswer(
+                new ImmutableMap.Builder<Pair<String, String>, ImmutableList<Cluster>>()
+                    .put(
+                        ImmutablePair.of(TEST_PROJECT_ID, TEST_ZONE), ImmutableList.of(testCluster))
+                    .put(
+                        ImmutablePair.of(OTHER_PROJECT_ID, TEST_ZONE),
+                        ImmutableList.of(otherCluster))
+                    .put(ImmutablePair.of(EMPTY_PROJECT_ID, TEST_ZONE), ImmutableList.of())
+                    .build()));
+    containerClient = new ContainerClient(container);
   }
 
   @Test
-  public void testGetClusterReturnsProperlyWhenClusterExists() throws Exception {
-    Cluster testCluster = new Cluster();
-    testCluster.setName("testCluster");
-    testCluster.setZone("us-central1-c");
-    Mockito.when(clusters.get(Matchers.anyString(), Matchers.anyString(), Matchers.anyString()))
-        .thenAnswer(
-            mockClustersGetAnswer(
-                new ImmutableMap.Builder<String, ImmutableList<Cluster>>()
-                    .put("testProject", ImmutableList.<Cluster>of(testCluster))
-                    .build()));
-
-    Cluster response = containerClient.getCluster("testProject", "us-central1-c", "testCluster");
+  public void testGetClusterReturnsProperlyWhenClusterExists() throws IOException {
+    Cluster response = containerClient.getCluster(TEST_PROJECT_ID, TEST_ZONE, "testCluster");
     assertNotNull(response);
-    assertEquals(response, testCluster);
+    assertEquals(testCluster, response);
   }
 
   @Test(expected = IOException.class)
-  public void testGetClusterThrowsErrorWhenClusterDoesntExists() throws Exception {
-    Cluster testCluster = new Cluster();
-    testCluster.setName("testCluster");
-    testCluster.setZone("us-central1-c");
-    Mockito.when(clusters.get(Matchers.anyString(), Matchers.anyString(), Matchers.anyString()))
-        .thenAnswer(
-            mockClustersGetAnswer(
-                new ImmutableMap.Builder<String, ImmutableList<Cluster>>()
-                    .put("testProject", ImmutableList.<Cluster>of(testCluster))
-                    .build()));
-
-    containerClient.getCluster("testProject", "us-central-c", "otherCluster");
+  public void testGetClusterThrowsErrorWhenClusterDoesntExists() throws IOException {
+    containerClient.getCluster(TEST_PROJECT_ID, OTHER_ZONE, "otherCluster");
   }
 
-  private Answer<Container.Projects.Zones.Clusters.Get> mockClustersGetAnswer(
+  @Test(expected = IllegalArgumentException.class)
+  public void testListClustersErrorWithNullProjectId() throws IOException {
+    testListClusters(null, TEST_ZONE, ImmutableList.of());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testListClustersErrorWithNullZone() throws IOException {
+    testListClusters(TEST_PROJECT_ID, null, ImmutableList.of());
+  }
+
+  @Test
+  public void testListClustersWithValidInputsWhenClustersExist() throws IOException {
+    testListClusters(TEST_PROJECT_ID, TEST_ZONE, ImmutableList.of(TEST_CLUSTER));
+  }
+
+  @Test
+  public void testListClustersWithDifferentProjectClusters() throws IOException {
+    testListClusters(OTHER_PROJECT_ID, TEST_ZONE, ImmutableList.of(OTHER_CLUSTER));
+  }
+
+  @Test
+  public void testListClustersWithValidInputsWhenClustersIsNull() throws IOException {
+    testListClusters(OTHER_PROJECT_ID, OTHER_ZONE, ImmutableList.of());
+  }
+
+  @Test
+  public void testListClustersEmptyWithValidProjectWithNoClusters() throws IOException {
+    testListClusters(EMPTY_PROJECT_ID, TEST_ZONE, ImmutableList.of());
+  }
+
+  @Test(expected = IOException.class)
+  public void testListClustersThrowsErrorWithInvalidProject() throws IOException {
+    testListClusters(ERROR_PROJECT_ID, TEST_ZONE, ImmutableList.of());
+  }
+
+  private static void testListClusters(String projectId, String zone, List<String> expected)
+      throws IOException {
+    List<Cluster> clusters = new ArrayList<>();
+    expected.forEach(e -> clusters.add(new Cluster().setName(e).setZone(TEST_ZONE)));
+    List<Cluster> response = containerClient.listClusters(projectId, zone);
+    assertNotNull(response);
+    assertEquals(ImmutableList.copyOf(clusters), response);
+  }
+
+  private static Answer<Container.Projects.Zones.Clusters.Get> mockClustersGetAnswer(
       ImmutableMap<String, ImmutableList<Cluster>> projectToClusters) {
-    return new Answer<Container.Projects.Zones.Clusters.Get>() {
-      @Override
-      public Container.Projects.Zones.Clusters.Get answer(InvocationOnMock invocation)
-          throws IOException {
-        Object[] args = invocation.getArguments();
-        String projectId = (String) args[0];
-        String zone = (String) args[1];
-        String clusterName = (String) args[2];
-        if (!projectToClusters.containsKey(projectId)) {
-          throw new IOException(
-              String.format(
-                  "Failed to find cluster, projectId: %s, zone: %s, cluster: %s",
-                  projectId, zone, clusterName));
-        }
-
-        Optional<Cluster> cluster =
-            projectToClusters
-                .get(projectId)
-                .stream()
-                .filter(c -> c.getName().equals(clusterName) && c.getZone().equals(zone))
-                .findFirst();
-        if (!cluster.isPresent()) {
-          throw new IOException(
-              String.format(
-                  "Failed to find cluster, projectId: %s, zone: %s, cluster: %s",
-                  projectId, zone, clusterName));
-        }
-
-        Container.Projects.Zones.Clusters.Get getCall =
-            Mockito.mock(Container.Projects.Zones.Clusters.Get.class);
-        Mockito.when(getCall.execute()).thenReturn(cluster.get());
-        return getCall;
+    return invocation -> {
+      Object[] args = invocation.getArguments();
+      String projectId = (String) args[0];
+      String zone = (String) args[1];
+      String clusterName = (String) args[2];
+      if (!projectToClusters.containsKey(projectId)) {
+        throw new IOException(
+            String.format(
+                "Failed to find cluster, projectId: %s, zone: %s, cluster: %s",
+                projectId, zone, clusterName));
       }
+
+      Optional<Cluster> cluster =
+          projectToClusters
+              .get(projectId)
+              .stream()
+              .filter(c -> c.getName().equals(clusterName) && c.getZone().equals(zone))
+              .findFirst();
+      if (!cluster.isPresent()) {
+        throw new IOException(
+            String.format(
+                "Failed to find cluster, projectId: %s, zone: %s, cluster: %s",
+                projectId, zone, clusterName));
+      }
+
+      Container.Projects.Zones.Clusters.Get getCall =
+          Mockito.mock(Container.Projects.Zones.Clusters.Get.class);
+      Mockito.when(getCall.execute()).thenReturn(cluster.get());
+      return getCall;
+    };
+  }
+
+  private static Answer<Container.Projects.Zones.Clusters.List> mockClustersListAnswer(
+      ImmutableMap<Pair<String, String>, ImmutableList<Cluster>> projectToClusters) {
+    return invocation -> {
+      Object[] args = invocation.getArguments();
+      String projectId = (String) args[0];
+      String zone = (String) args[1];
+      Pair<String, String> parent = ImmutablePair.of(projectId, zone);
+
+      List<Cluster> clusters = null;
+      if (projectToClusters.containsKey(parent)) {
+        clusters = new ArrayList<>(projectToClusters.get(parent));
+      } else if (ERROR_PROJECT_ID.equals(projectId)) {
+        throw new IOException(
+            String.format(
+                "Failed to get clusters for projectId: %s and zone: %s", projectId, zone));
+      }
+
+      Container.Projects.Zones.Clusters.List listCall =
+          Mockito.mock(Container.Projects.Zones.Clusters.List.class);
+      ListClustersResponse response = new ListClustersResponse().setClusters(clusters);
+      Mockito.when(listCall.execute()).thenReturn(response);
+      return listCall;
     };
   }
 }
