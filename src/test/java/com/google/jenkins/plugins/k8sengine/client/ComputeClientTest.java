4ea110918404f97ee926104e3bc8f62351d97003
Stephen Shank <stephenshank@google.com>
2019-03-05 15:50:31 -0800

    Simplify compute client test setup with Answer. Rename test for clarity. Add new test case to separate sorting and return behavior. Use constants for zones.

--- src/test/java/com/google/jenkins/plugins/k8sengine/client/ComputeClientTest.java
+++ src/test/java/com/google/jenkins/plugins/k8sengine/client/ComputeClientTest.java
@@ -16,12 +16,14 @@ package com.google.jenkins.plugins.k8sengine.client;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import com.google.api.services.compute.Compute;
 import com.google.api.services.compute.Compute.Zones;
 import com.google.api.services.compute.model.Zone;
 import com.google.api.services.compute.model.ZoneList;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,47 +34,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 /** Test {@link ComputeClient}. */
 @RunWith(MockitoJUnitRunner.class)
 public class ComputeClientTest {
   private static final String TEST_PROJECT_ID = "test-project";
+  private static final String OTHER_PROJECT_ID = "other-project";
   private static final String NULL_PROJECT_ID = "wrong-project";
   private static final String EMPTY_PROJECT_ID = "empty-project-id";
   private static final String ERROR_PROJECT_ID = "error-project-id";
+  private static final String TEST_ZONE_A = "us-central1-a";
+  private static final String TEST_ZONE_B = "us-west1-b";
+  private static final String TEST_ZONE_C = "us-east1-c";
   private static final List<String> ZONE_NAMES =
-      Arrays.asList("us-west-1b", "us-central-1a", "us-east-1c");
+      Arrays.asList(TEST_ZONE_B, TEST_ZONE_A, TEST_ZONE_C);
   private static ComputeClient computeClient;
 
   @BeforeClass
   public static void init() throws IOException {
-    List<Zone> listOfZones = initZoneList(ZONE_NAMES);
-    // Mock zones
     Compute compute = Mockito.mock(Compute.class);
-
     Zones zones = Mockito.mock(Zones.class);
-    Zones.List zonesListCall = Mockito.mock(Zones.List.class);
-    ZoneList zoneList = Mockito.mock(ZoneList.class);
     Mockito.when(compute.zones()).thenReturn(zones);
-    Mockito.when(zones.list(TEST_PROJECT_ID)).thenReturn(zonesListCall);
-    Mockito.when(zonesListCall.execute()).thenReturn(zoneList);
-    Mockito.when(zoneList.getItems()).thenReturn(listOfZones);
-
-    Zones.List nullZonesListCall = Mockito.mock(Zones.List.class);
-    ZoneList nullZoneList = Mockito.mock(ZoneList.class);
-    Mockito.when(zones.list(NULL_PROJECT_ID)).thenReturn(nullZonesListCall);
-    Mockito.when(nullZonesListCall.execute()).thenReturn(nullZoneList);
-    Mockito.when(nullZoneList.getItems()).thenReturn(null);
-
-    Zones.List emptyZonesListCall = Mockito.mock(Zones.List.class);
-    ZoneList emptyZoneList = Mockito.mock(ZoneList.class);
-    Mockito.when(zones.list(EMPTY_PROJECT_ID)).thenReturn(emptyZonesListCall);
-    Mockito.when(emptyZonesListCall.execute()).thenReturn(emptyZoneList);
-    Mockito.when(emptyZoneList.getItems()).thenReturn(ImmutableList.of());
-
-    Zones.List errorZonesListCall = Mockito.mock(Zones.List.class);
-    Mockito.when(zones.list(ERROR_PROJECT_ID)).thenReturn(errorZonesListCall);
-    Mockito.when(errorZonesListCall.execute()).thenThrow(new IOException());
+    Mockito.when(zones.list(anyString()))
+        .thenAnswer(
+            mockZonesListAnswer(
+                new ImmutableMap.Builder<String, List<Zone>>()
+                    .put(TEST_PROJECT_ID, initZoneList(ZONE_NAMES))
+                    .put(OTHER_PROJECT_ID, initZoneList(ImmutableList.of(TEST_ZONE_C)))
+                    .put(EMPTY_PROJECT_ID, ImmutableList.of())
+                    .build()));
     computeClient = new ComputeClient(compute);
   }
 
@@ -101,7 +92,14 @@ public class ComputeClientTest {
   }
 
   @Test
-  public void testGetZonesReturnsAllSortedWhenProjectIdCorrect() throws IOException {
+  public void testGetZonesReturnsWhenSingleZone() throws IOException {
+    List<Zone> zones = computeClient.getZones(OTHER_PROJECT_ID);
+    assertNotNull(zones);
+    assertEquals(initZoneList(ImmutableList.of(TEST_ZONE_C)), zones);
+  }
+
+  @Test
+  public void testGetZonesReturnsAllSortedWhenMultipleZones() throws IOException {
     List<Zone> expectedZones = initZoneList(ZONE_NAMES);
     expectedZones.sort(Comparator.comparing(Zone::getName));
     List<Zone> zones = computeClient.getZones(TEST_PROJECT_ID);
@@ -113,5 +111,24 @@ public class ComputeClientTest {
     List<Zone> result = new ArrayList<>();
     names.forEach(z -> result.add(new Zone().setName(z)));
     return result;
+  }
+
+  private static Answer<Zones.List> mockZonesListAnswer(
+      ImmutableMap<String, List<Zone>> projectIdToZones) {
+    return invocation -> {
+      Object[] args = invocation.getArguments();
+      String projectId = (String) args[0];
+
+      Zones.List listCall = Mockito.mock(Zones.List.class);
+      if (projectIdToZones.containsKey(projectId)) {
+        List<Zone> zones = new ArrayList<>(projectIdToZones.get(projectId));
+        Mockito.when(listCall.execute()).thenReturn(new ZoneList().setItems(zones));
+      } else if (ERROR_PROJECT_ID.equals(projectId)) {
+        Mockito.when(listCall.execute()).thenThrow(new IOException());
+      } else {
+        Mockito.when(listCall.execute()).thenReturn(new ZoneList().setItems(null));
+      }
+      return listCall;
+    };
   }
 }
