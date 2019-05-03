01c8e7a57648f274ea3ec50b2b5f6e27eb38d866
Stephen Shank <stephenshank@google.com>
2019-05-02 18:56:11 -0700

    PR feedback: Use helper method for defining mock manifest files. Add tests for the case where multiple files are used with different namespaces.

--- src/test/java/com/google/jenkins/plugins/k8sengine/KubernetesEngineBuilderNamespaceTest.java
+++ src/test/java/com/google/jenkins/plugins/k8sengine/KubernetesEngineBuilderNamespaceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 
+import com.google.common.base.Strings;
 import hudson.FilePath;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -15,139 +16,100 @@ import org.yaml.snakeyaml.Yaml;
 
 public class KubernetesEngineBuilderNamespaceTest {
   @Test
-  public void testAddStarNamespaceWithNoPreviousNamespace()
+  public void testAddStarNamespaceWithNoManifestNamespace()
       throws IOException, InterruptedException {
-    FilePath manifestFile = Mockito.mock(FilePath.class);
-    Mockito.when(manifestFile.read())
-        .thenReturn(
-            new ByteArrayInputStream(
-                String.join(
-                        "\n",
-                        "apiVersion: apps/v1",
-                        "kind: Deployment",
-                        "metadata:",
-                        "  name: nginx-deployment",
-                        "  labels:",
-                        "     app: nginx")
-                    .getBytes()));
-    Mockito.doAnswer(
-            invocation -> {
-              Yaml yaml = new Yaml();
-              Manifests.ManifestObject manifest =
-                  new Manifests.ManifestObject(
-                      yaml.load((String) invocation.getArguments()[0]), manifestFile);
-              Optional<String> namespace = manifest.getNamespace();
-              assertNotNull(namespace);
-              assertTrue(namespace.isPresent());
-              assertEquals("default", namespace.get());
-              return null;
-            })
-        .when(manifestFile)
-        .write(anyString(), anyString());
+    FilePath manifestFile = setupManifest("", "default");
     String commandNameSpace = KubernetesEngineBuilder.addNamespace(manifestFile, "*");
     assertEquals("default", commandNameSpace);
   }
 
   @Test
-  public void testAddStarNamespaceWithExistingNamespace() throws IOException, InterruptedException {
-    FilePath manifestFile = Mockito.mock(FilePath.class);
-    Mockito.when(manifestFile.read())
-        .thenReturn(
-            new ByteArrayInputStream(
-                String.join(
-                        "\n",
-                        "apiVersion: apps/v1",
-                        "kind: Deployment",
-                        "metadata:",
-                        "  name: nginx-deployment",
-                        "  namespace: test",
-                        "  labels:",
-                        "     app: nginx")
-                    .getBytes()));
-    Mockito.doAnswer(
-            invocation -> {
-              Yaml yaml = new Yaml();
-              Manifests.ManifestObject manifest =
-                  new Manifests.ManifestObject(
-                      yaml.load((String) invocation.getArguments()[0]), manifestFile);
-              Optional<String> namespace = manifest.getNamespace();
-              assertNotNull(namespace);
-              assertTrue(namespace.isPresent());
-              assertEquals("test", namespace.get());
-              return null;
-            })
-        .when(manifestFile)
-        .write(anyString(), anyString());
+  public void testAddStarNamespaceWithManifestNamespace() throws IOException, InterruptedException {
+    FilePath manifestFile = setupManifest("test", "test");
     String commandNameSpace = KubernetesEngineBuilder.addNamespace(manifestFile, "*");
     assertEquals("test", commandNameSpace);
   }
 
   @Test
-  public void testAddCustomNamespaceWithNoPreviousNamespace()
+  public void testAddCustomNamespaceWithNoManifestNamespace()
       throws IOException, InterruptedException {
-    FilePath manifestFile = Mockito.mock(FilePath.class);
-    Mockito.when(manifestFile.read())
-        .thenReturn(
-            new ByteArrayInputStream(
-                String.join(
-                        "\n",
-                        "apiVersion: apps/v1",
-                        "kind: Deployment",
-                        "metadata:",
-                        "  name: nginx-deployment",
-                        "  labels:",
-                        "     app: nginx")
-                    .getBytes()));
-    Mockito.doAnswer(
-            invocation -> {
-              Yaml yaml = new Yaml();
-              Manifests.ManifestObject manifest =
-                  new Manifests.ManifestObject(
-                      yaml.load((String) invocation.getArguments()[0]), manifestFile);
-              Optional<String> namespace = manifest.getNamespace();
-              assertNotNull(namespace);
-              assertTrue(namespace.isPresent());
-              assertEquals("test", namespace.get());
-              return null;
-            })
-        .when(manifestFile)
-        .write(anyString(), anyString());
+    FilePath manifestFile = setupManifest("", "test");
     String commandNameSpace = KubernetesEngineBuilder.addNamespace(manifestFile, "test");
     assertEquals("test", commandNameSpace);
   }
 
   @Test
-  public void testAddCustomNamespaceWithPreviousNamespace()
+  public void testAddCustomNamespaceWithManifestNamespace()
+      throws IOException, InterruptedException {
+    FilePath manifestFile = setupManifest("random", "default");
+    String commandNameSpace = KubernetesEngineBuilder.addNamespace(manifestFile, "default");
+    assertEquals("default", commandNameSpace);
+  }
+
+  @Test
+  public void testAddStarNamespaceWithMultipleManifestNamespaces()
+      throws IOException, InterruptedException {
+    FilePath manifestPath = Mockito.mock(FilePath.class);
+    FilePath testManifest = setupManifest("test", "test");
+    FilePath defaultManifest = setupManifest("default", "default");
+    Mockito.when(manifestPath.isDirectory()).thenReturn(true);
+    FilePath[] manifests = {testManifest, defaultManifest};
+    Mockito.when(manifestPath.list(anyString())).thenReturn(manifests);
+    String commandNamespace = KubernetesEngineBuilder.addNamespace(manifestPath, "*");
+    assertEquals("", commandNamespace);
+  }
+
+  @Test
+  public void testAddCustomNamespaceWithMultipleManifestNamespaces()
+      throws IOException, InterruptedException{
+    FilePath manifestPath = Mockito.mock(FilePath.class);
+    FilePath testManifest = setupManifest("test", "custom");
+    FilePath defaultManifest= setupManifest("default", "custom");
+    Mockito.when(manifestPath.isDirectory()).thenReturn(true);
+    FilePath[] manifests = {testManifest, defaultManifest};
+    Mockito.when(manifestPath.list(anyString())).thenReturn(manifests);
+    String commandNamespace = KubernetesEngineBuilder.addNamespace(manifestPath, "custom");
+    assertEquals("custom", commandNamespace);
+  }
+
+  /**
+   * @param inputNamespace The namespace to be included in the original file contents if this is
+   *     null or "", then no namespace line will be included
+   * @param expectedNamespace The namespace that should be included on the object after writing.
+   * @return The mocked FilePath for this manifest
+   * @throws IOException Included because of the mocked use of read and write
+   * @throws InterruptedException Included because of the mocked use of read and write
+   */
+  private FilePath setupManifest(String inputNamespace, String expectedNamespace)
       throws IOException, InterruptedException {
     FilePath manifestFile = Mockito.mock(FilePath.class);
+    StringBuilder builder =
+        new StringBuilder()
+            .append("apiVersion: apps/v1\n")
+            .append("kind: Deployment\n")
+            .append("metadata:\n")
+            .append("  name: nginx-deployment\n")
+            .append("  labels:\n")
+            .append("    app: nginx\n");
+    if (!Strings.isNullOrEmpty(inputNamespace)) {
+      builder.append("  namespace: ").append(inputNamespace);
+    }
     Mockito.when(manifestFile.read())
-        .thenReturn(
-            new ByteArrayInputStream(
-                String.join(
-                        "\n",
-                        "apiVersion: apps/v1",
-                        "kind: Deployment",
-                        "metadata:",
-                        "  name: nginx-deployment",
-                        "  namespace: random",
-                        "  labels:",
-                        "     app: nginx")
-                    .getBytes()));
+        .thenReturn(new ByteArrayInputStream(builder.toString().getBytes()));
     Mockito.doAnswer(
             invocation -> {
               Yaml yaml = new Yaml();
               Manifests.ManifestObject manifest =
                   new Manifests.ManifestObject(
                       yaml.load((String) invocation.getArguments()[0]), manifestFile);
-              Optional<String> namespace = manifest.getNamespace();
-              assertNotNull(namespace);
-              assertTrue(namespace.isPresent());
-              assertEquals("default", namespace.get());
+              Optional<String> actualNamespace = manifest.getNamespace();
+              assertNotNull(actualNamespace);
+              assertTrue(actualNamespace.isPresent());
+              assertEquals(expectedNamespace, actualNamespace.get());
               return null;
             })
         .when(manifestFile)
         .write(anyString(), anyString());
-    String commandNameSpace = KubernetesEngineBuilder.addNamespace(manifestFile, "default");
-    assertEquals("default", commandNameSpace);
+    return manifestFile;
   }
 }
